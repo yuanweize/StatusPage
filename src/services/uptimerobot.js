2c8330473056006ca46abed7bad2dc1b0fc00828
翠花 <admin@nange.cn>
2020-11-24 15:24:38 +0800

    fix

--- src/services/uptimerobot.js
+++ src/services/uptimerobot.js
@@ -12,10 +12,10 @@ function lastDays(distance) {
   const ranges = [];
   const getTime = date => Math.floor(date.getTime() / 1000);
 
-for (let i = distance; i > 0; i--) {
+for (let i = -distance; i <= 0; i++) {
     const day0 = addDays(now, i);
     const day1 = addSeconds(addDays(day0, 1), -1);
-    dates.push(format(day0, "MMM Do, YYYY"));
+    dates.push(format(day0, "YYYY年MM月DD日"));
     ranges.push(`${getTime(day0)}_${getTime(day1)}`);
   }
   return { dates, ranges: ranges.join("-") };
